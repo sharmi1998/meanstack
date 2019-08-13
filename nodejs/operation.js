d6ddb33215f248008cbbf08f4246f614f0435555
sharmi1998 <gayu438@gmail.com>
2019-08-13 15:00:33 +0530

    node programs

---
+++ nodejs/operation.js
@@ -0,0 +1,16 @@
+module.exports.add=(x,y)=>{
+    var z=x+y;
+    return z;
+}
+module.exports.sub=(x,y)=>{
+    var w=x-y;
+    return w;
+}
+module.exports.mul=(x,y)=>{
+    var q=x*y;
+    return q;
+}
+module.exports.div=(x,y)=>{
+    var e=x/y;
+    return e;
+}
